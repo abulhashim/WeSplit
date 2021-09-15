104771b119b58f6a44f7cf12eca8e54411c543ec
abulhashim <abulhashim351@gmail.com>
2021-09-15 21:52:59 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by MacBook Pro on 15/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
